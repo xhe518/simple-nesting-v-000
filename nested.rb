8445ce56bbbdb375dfcf42db742ea5549c4cf1a2
Sid He <xhe518@gmail.com>
2018-08-08 05:25:58 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,10 +85,12 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] =
-	{:known_for => "Ruby",
-		:languages => ["LISP", "C"]
-	}
+	programmer_hash[:yukihiro_matsumoto] =
+		{:known_for => "Ruby",
+			:languages => ["LISP", "C"]
+		}
+
+	programmer_hash
 end
 
 def changing_alan
@@ -132,4 +134,6 @@ def adding_to_dennis
         }
      }
 
+	programmer_hash[:dennis_ritchie][:languages] <= "Assembly"
+	programmer_hash
 end
