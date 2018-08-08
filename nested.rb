1af3577c1f1dc07900568084d1a68f7f322619a0
Sid He <xhe518@gmail.com>
2018-08-08 05:27:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,6 @@ def adding_to_dennis
         }
      }
 
-	programmer_hash[:dennis_ritchie][:languages] <= "Assembly"
+	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 	programmer_hash
 end
